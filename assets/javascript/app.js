4f5205117f261467aecd4c1cb5725706ab2dc2db
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-05-08 01:08:34 -0400

    Game done V1.0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,36 +16,43 @@ var triviaGame = {
         question: "Batman had many sidekicks over the years that took on the identity of Robin. Who was the first Robin?",
         choices: ["Richard Grayson","Jason Todd","Tim Drake","Damian Wayne"],
         answer: 0,
+        image: "robins__earth_27__by_phil_cho-daadbdv.jpg",
     },
     q2: {
         question: "In the original ReBoot TV series (1994 - 2001), what is the name of the super-virius that was created when the viruses Megabyte and Hexidecimal merged?",
         choices: ["Daemon","Gigabyte", "Killabyte", "Glitch"],
         answer: 1,
+        image: "Gigabyte.jpg",
     },
     q3: {
         question: "True or False: Iron Man's armor in the Marvel Cinematic Universe is made out of Adamantium.",
         choices: ["True","False"],
         answer: 1,
+        image: "iron_man.jpg",
     },
     q4: {
         question: "What are the names of Thomas passenger coaches in Thomas the Tank Engine?",
         choices: ["Rom & Ram","Alice & Allison","Rod & Todd","Annie & Clarabel"],
         answer: 3,
+        image: "Annieandclarabel.jpg",
     },
     q5: {
         question: "Which of the following is not a name of a NASA Space Shuttle Orbiter",
         choices: ["Discovery","Enterprise","Odyssey","Atlantis"],
         answer: 2,
+        image: "shuttle.jpg",
     },
     q6: {
         question: "In the movie The Matrix (1999), which pill did Neo take?",
         choices: ["Red Pill","Blue Pill"],
         answer: 0,
+        image: "matrix.jpg",
     },
     q7: {
         question: "What is the name of Monopoly's mascot Mr.Monopoly?",
-        choices: ["Arthur Monopoly III","Lorenzo Von Matterhorn", "Rich Uncle Pennybags", "Uncle Moneybags"],
+        choices: ["Richie Monopoly III","Lorenzo Von Matterhorn", "Rich Uncle Pennybags", "Uncle Moneybags"],
         answer: 2,
+        image: "mr-monopoly.jpg",
     },
 
     gameReset: function(){
@@ -68,7 +75,7 @@ var triviaGame = {
             //Go to time-out
             $("#timeLimit").text(timer);
             triviaGame.loading = !triviaGame.loading;
-            timer = 2;
+            timer = resultTime;
             triviaGame.timeOut();
         }
         else if (timer <= 0 && !triviaGame.loading){
@@ -81,14 +88,13 @@ var triviaGame = {
             }
             else{
                 //Sets up next trivia question
-                timer = 5;
+                timer = questionTime;
                 triviaGame.triviaSetup();
             }        
         }
         else{
             //DEBUG CODE
-            console.log(timer);
-
+            //console.log(timer);
             //Display time limit
             $("#timeLimit").text(timer);
             timer--;
@@ -98,8 +104,7 @@ var triviaGame = {
     timeOut: function(){
         //Object Function: User did not choose an answer within the time limit
         //DEBUG CODE
-        console.log(questionList[triviaGame.gameCount].answer);
-
+        //console.log(questionList[triviaGame.gameCount].answer);
         triviaGame.showAnswer("TIMES UP!")
         triviaGame.unanswered++;
         triviaGame.gameCount++; //increment counter
@@ -107,7 +112,7 @@ var triviaGame = {
 
     triviaSetup: function(){
         //Object Function: Setup and displays question and answer choices
-        $("#gamePhase").text("Question: ");
+        $("#gamePhase").text("QUESTION: ");
         $("#question").text(questionList[triviaGame.gameCount].question);
 
         $("#choices").empty();
@@ -115,23 +120,23 @@ var triviaGame = {
             var label = questionList[triviaGame.gameCount].choices[i];
             $('#choices').append("<button class='btn btn-primary btn-block answer' value=" + i + ">" + label + "</button>")
         }
-
         //DEBUG CODE
-        console.log(questionList[triviaGame.gameCount].question);
+        //console.log(questionList[triviaGame.gameCount].question);
     },
 
     compareAnswers: function(userAnswer){
         //Object Function: Compare userAnswer (int) with true answer (int)
         if (userAnswer == questionList[triviaGame.gameCount].answer){
             //DEBUG CODE
-            console.log("correct");
+            //console.log("correct");
             triviaGame.right++;
             triviaGame.showAnswer("CORRECT!");
 
         }
         else{
             //DEBUG CODE
-            console.log("incorrect")
+            //console.log("incorrect")
+
             triviaGame.wrong++;
             triviaGame.showAnswer("WRONG!")
         }
@@ -140,14 +145,25 @@ var triviaGame = {
         $("#gamePhase").text(result);
         $("#question").text("The answer was: " + questionList[triviaGame.gameCount].choices[questionList[triviaGame.gameCount].answer]);
         $("#choices").empty();
+        var img = $("<img>");
+        img.attr({
+            src: ("assets/images/" + questionList[triviaGame.gameCount].image),
+            alt: "image",
+            class: "img-fluid imgBlock"
+        });
+        var imgContainer = $("<div>");
+        imgContainer.addClass("imgContainer");
+        imgContainer.append(img);
+        $("#choices").append(imgContainer);
     }
 };
 
 //Global variables
 var gameInterval; //Variable to hold setInterval that runs the game
-
 var questionList = [triviaGame.q1, triviaGame.q2, triviaGame.q3, triviaGame.q4, triviaGame.q5, triviaGame.q6, triviaGame.q7];
 var timer;
+var questionTime = 10;
+var resultTime = 3;
 
 //Functions
 //Note: experimenting with ES6 syntax
@@ -155,9 +171,9 @@ const gameStart = () => {
     //Funtion: Start interval timer
 
     //DEBUG CODE
-    console.log("game start");
+    //console.log("game start");
 
-    timer = 5;
+    timer = questionTime;
     clearInterval(gameInterval);
     gameInterval = setInterval(triviaGame.nextQuestion, 1000);
     triviaGame.triviaSetup();
@@ -171,13 +187,14 @@ const gameStop = () => {
     $("#clock").css("display", "none");
     $("#gamePhase").text("RESULTS");
     $("#question").empty();
+    $("#choices").empty();
     $("#correct").text("correct: " + triviaGame.right);
     $("#incorrect").text("incorrect: " + triviaGame.wrong);
     $("#unanswered").text("unanswered: " + triviaGame.unanswered);
     $("#playAgain").css("display","block");
 
     //DEBUG CODE
-    console.log("game stopped");
+    //console.log("game stopped");
 }
 
 //Main
@@ -186,20 +203,18 @@ $(document).ready(function() {
     $("#start").on("click", function(){
         //Start game after start button clicked
         triviaGame.gameReset();
-
-        //TODO: stuff for init game setup
         $("#start").css("display", "none");
         $("#clock").css("display", "block");
+        $("#question-block").css("display", "block");
     });
 
     $("#choices").on("click", ".answer", function(){
         //Resets the timer to display player choice result
-        timer = 3;
+        timer = resultTime;
         triviaGame.loading = false;
 
         //DEBUG CODE
-        console.log("clicked " + this.value);
-
+        //console.log("clicked " + this.value);
         triviaGame.compareAnswers(this.value);
         triviaGame.gameCount++; //increment counter
     })
